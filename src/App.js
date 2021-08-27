89987d7dbaaec99ebc256a58e8dfc107ba16730d
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-27 02:42:23 -0400

    Adding React bootstrap stylesheets in index.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import ContactDetails from './components/ContactDetails';
 import AddContactDetails from './components/AddContactDetails';
