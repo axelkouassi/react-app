5f5ca318586c89337fc40f2b29f268ea43859b67
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-30 03:16:36 -0400

    Using functional component to handle filtering of data | adding methods to update, remove, and filtering | Displaying only header, search bar, contact form and contact list

--- src/App.js
+++ src/App.js
@@ -24,13 +24,18 @@ function App() {
     }
 ]); */
 
-const [contactDetails, setContactDetails]=useState([]);
+const [contactDetails, setContactDetails] = useState([]);
+const [searchTerm, setSearchTerm] = useState("");
+const [searchList, setSearchList] = useState([]);
 
 useEffect(() => {
   const retievedData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '[]');
   console.log("In useEffect 1, retrieving data..."+ retievedData);
   if(retievedData){
     setContactDetails(retievedData);
+  }
+  if(contactDetails.length === 0){
+    console.log("In contactDetails length == 0")
   }
 }, []);
 
@@ -47,16 +52,77 @@ const addContactData = (person)=>{
   console.log(contactDetails);
 }
 
+const searchHandler = (searchText)=>{
+  console.log("In searchHandler");
+  setSearchTerm(searchText);
+  if(searchTerm !== ""){
+    const searchContact = contactDetails.filter((contact)=>{
+      console.log("In filter contact");
+      console.log(Object.values(contact));
+      const str = Object.values(contact) //convert object into an array of values
+      .join(" ")
+      .toLowerCase().includes(searchTerm);
+      /* const status = str.includes(searchTerm.toLowerCase); */
+      console.log("Object converted into array of strings: " + str);
+      console.log("Search term : " + searchTerm);
+      /* console.log("Status :" + status); */
+      
+    });
+
+    setSearchList(searchContact);
+
+  }else{
+    setSearchList(contactDetails);
+  }
+
+  /* return searchList; */
+}
+
+const updateContactData = (contact)=>{
+  console.log("In updateContactData");
+  console.log(contact);
+  setContactDetails(
+    contactDetails.map((c)=>{
+      return c.id === contact.id?{...contact}:c;
+    })
+  )
+}
+
+const removeContactData = (id)=>{
+  const delContact = contactDetails.filter(c=>{
+    return c.id!== id;
+  });
+  setContactDetails(delContact);
+}
+
 
   return (
     <div className="App">
       <Header/>
-      <ContactFinder contacts={contactDetails}/>
-      <hr/>
+
+      <ContactFinder /* key={JSON.stringify(contactDetails)} */ 
+                     contacts={searchTerm.length<1?contactDetails:searchList}
+                     searchText={searchTerm}
+                     searchKeyword={searchHandler}
+                     getContactId={removeContactData}
+      />
       <AddContactDetails addContactHandler={addContactData}/>
-      <ContactCard card={contactDetails}/>
-      <PersonListIcon cardIcon={contactDetails}/>
-      <ContactDetails details={contactDetails}/>
+      {/* <ContactCard card={searchTerm.length<1?contactDetails:searchList}
+                   searchText={searchTerm}
+                   searchKeyword={searchHandler}
+                   getContactId={removeContactData}
+      /> */}
+      <hr/>
+      <PersonListIcon cardIcon={searchTerm.length<1?contactDetails:searchList}
+                      searchText={searchTerm}
+                      searchKeyword={searchHandler}
+                      getContactId={removeContactData}
+      />
+      {/* <ContactDetails details={searchTerm.length<1?contactDetails:searchList}
+                      searchText={searchTerm}
+                      searchKeyword={searchHandler}
+                      getContactId={removeContactData}
+      /> */}
     </div>
   );
 }
